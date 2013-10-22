463da0f41c5904705135e695a0d065f471286605
Colin Thomson <colthomson@hotmail.com>
2013-10-21 21:04:12 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Colin Thomson"
-		email "colthomson@hotmail.com"
-		password "monkeys"
-		password_confirmation "monkeys"
+		sequence(:name)	 { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
+		password "foobar"
+		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
